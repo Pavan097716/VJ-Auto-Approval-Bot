b198f4b1c26d33b83df397c2c0829eb40a7c4f92
Pavan097716 <pavanvallu185@gmail.com>
2025-06-15 13:31:58 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -6,14 +6,14 @@
 from os import path, getenv
 
 class Config:
-    API_ID = int(getenv("API_ID", ""))
-    API_HASH = getenv("API_HASH", "")
-    BOT_TOKEN = getenv("BOT_TOKEN", "")
+    API_ID = int(getenv("API_ID", "20793620"))
+    API_HASH = getenv("API_HASH", "a712d2b8486f26c4dee5127cc9ae0615")
+    BOT_TOKEN = getenv("BOT_TOKEN", "7610534695:AAETk26NvwYon8Vm0d--22iUpoK0FkhdM6U")
     # Your Force Subscribe Channel Id Below 
-    CHID = int(getenv("CHID", "")) # Make Bot Admin In This Channel
+    CHID = int(getenv("CHID", "-1002330243493")) # Make Bot Admin In This Channel
     # Admin Or Owner Id Below
-    SUDO = list(map(int, getenv("SUDO", "").split()))
-    MONGO_URI = getenv("MONGO_URI", "")
+    SUDO = list(map(int, getenv("SUDO", "7825330156 1720910522").split()))
+    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://baskarkass70:<G84z76PCCDuGCmnd@cluster0.u3oyyfa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     
 cfg = Config()
 
